330873c636e1db8dd03dbf6faef8e14c81f26ee1
kishk <mrsaytama000@gmail.com>
2023-07-17 15:34:34 +0300

    complete the login and color of cells and keys

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
-import { colors, keys } from "./src/constants";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+import { colors, keys, CLEAR, ENTER } from "./src/constants";
 import Keyboard from "./src/components/Keyboard";
-
+import CustomAlert from "./src/components/CustomAlert";
 const NUMBER_OF_TRIES = 6;
 
 const copyArray = (arr) => {
@@ -19,51 +27,148 @@ export default function App() {
     new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill(""))
   );
 
+  // custome alert
+  const [showAlert, setShowAlert] = useState(false);
+  const handleShowAlert = () => {
+    setShowAlert(true);
+  };
+
+  const handleCloseAlert = () => {
+    setShowAlert(false);
+  };
   const [curRow, setCurRow] = useState(0);
   const [curCol, setCurCol] = useState(0);
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
 
+  useEffect(() => {
+    if (curRow > 0) {
+      checkGameState();
+    }
+  }, [curRow]);
+
+  const checkGameState = () => {
+    if (checkIfWin()) {
+      setTitle("Heeeh");
+      setMessage("W I N N E R🎉,come everyday");
+      setShowAlert(true);
+    } else if (checkIfLose()) {
+      setTitle("Eeew");
+      setMessage("L O S E R😔,try again tomorrow");
+      setShowAlert(true);
+    }
+  };
+  const checkIfWin = () => {
+    const row = rows[curRow - 1];
+
+    return row.every((letter, i) => letter === letters[i]);
+  };
+  const checkIfLose = () => {
+    return curRow === rows.length;
+  };
+  // const onKeyPressed = (key) => {
+  //   const updatedRows = copyArray(rows);
+
+  //   console.log(curCol, " ", curRow);
+  //   if (key === "CLEAR") {
+  //     if (curCol <= 0) {
+  //       setCurRow(curRow);
+  //       setCurCol(curCol);
+  //     } else {
+  //       setCurCol(curCol - 1);
+  //     }
+  //     updatedRows[curRow][curCol - 1] = "";
+  //   } else {
+  //     if (curCol < 4) setCurCol(curCol + 1);
+  //     else {
+  //       setCurCol(0);
+  //       setCurRow(curRow + 1);
+  //     }
+  //     updatedRows[curRow][curCol] = key;
+  //   }
+  //   setRows(updatedRows);
+  // };
   const onKeyPressed = (key) => {
     const updatedRows = copyArray(rows);
 
-    console.log(curCol, " ", curRow);
-    if (key === "CLEAR") {
-      if (curCol <= 0) {
-        setCurRow(curRow);
-        setCurCol(curCol);
-      } else {
-        setCurCol(curCol - 1);
+    if (key === CLEAR) {
+      const prevCol = curCol - 1;
+      if (prevCol >= 0) {
+        updatedRows[curRow][prevCol] = "";
+        setRows(updatedRows);
+        setCurCol(prevCol);
       }
-      updatedRows[curRow][curCol - 1] = "";
-    } else {
-      if (curCol < 4) setCurCol(curCol + 1);
-      else {
-        setCurCol(0);
-        setCurRow(curRow + 1);
-      }
-      updatedRows[curRow][curCol] = key;
+      return;
     }
-    setRows(updatedRows);
+
+    if (key === ENTER) {
+      if (curCol === rows[0].length) {
+        setCurRow(curRow + 1);
+        setCurCol(0);
+      }
+
+      return;
+    }
+
+    if (curCol < rows[0].length) {
+      updatedRows[curRow][curCol] = key;
+      setRows(updatedRows);
+      setCurCol(curCol + 1);
+    }
   };
 
   const isActiveCell = (row, col) => {
     return row === curRow && col === curCol;
   };
+
+  const getCellBGColor = (row, col) => {
+    const letter = rows[row][col];
+    console.log(row, curRow);
+    if (row >= curRow) {
+      return colors.black;
+    }
+    if (letter === letters[col]) {
+      return colors.primary;
+    }
+    if (letters.includes(letter)) {
+      return colors.secondary;
+    }
+    return colors.darkgrey;
+  };
+
+  const getAllColors = (color) => {
+    return rows.flatMap((row, i) =>
+      row.filter((cell, j) => getCellBGColor(i, j) === color)
+    );
+  };
+  const greenCaps = getAllColors(colors.primary);
+  const yellowCaps = getAllColors(colors.secondary);
+  const greyCaps = getAllColors(colors.darkgrey);
+
   return (
     <SafeAreaView style={styles.container}>
+      <CustomAlert
+        visible={showAlert}
+        title={title}
+        message={message}
+        onClose={handleCloseAlert}
+      />
       <StatusBar style="light" />
       <Text style={styles.title}>WORLDE</Text>
 
       <ScrollView style={styles.map}>
         {rows.map((row, i) => (
-          <View style={styles.row}>
+          <View key={`row-${i}`} style={styles.row}>
             {row.map((cell, j) => (
               <View
+                key={`cell-${i}-${j}`}
                 style={[
                   styles.cell,
                   {
                     borderColor: isActiveCell(i, j)
                       ? colors.lightgrey
                       : colors.darkgrey,
+                    backgroundColor: getCellBGColor(i, j),
                   },
                 ]}
               >
@@ -73,7 +178,12 @@ export default function App() {
           </View>
         ))}
       </ScrollView>
-      <Keyboard onKeyPressed={onKeyPressed} />
+      <Keyboard
+        onKeyPressed={onKeyPressed}
+        greenCaps={greenCaps}
+        yellowCaps={yellowCaps}
+        greyCaps={greyCaps}
+      />
     </SafeAreaView>
   );
 }
@@ -92,7 +202,6 @@ const styles = StyleSheet.create({
     letterSpacing: 7,
   },
   map: {
-    height: 100,
     alignSelf: "stretch",
     marginVertical: 20,
   },
---
+++ src/components/CustomAlert.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import Modal from "react-native-modal";
+
+const CustomAlert = ({ visible, title, message, onClose }) => {
+  return (
+    <Modal isVisible={visible} animationIn="zoomIn" animationOut="zoomOut">
+      <View style={styles.container}>
+        <View style={styles.alertContainer}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.message}>{message}</Text>
+          <TouchableOpacity style={styles.button} onPress={onClose}>
+            <Text style={styles.buttonText}>OK</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  alertContainer: {
+    backgroundColor: "#FFFFFF",
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  message: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#007AFF",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    minWidth: 100,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
+
+export default CustomAlert;
